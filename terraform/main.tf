5f1e6d6951f15c44fbd64dcb40058aef6a1733ed
Zerrelius <Jeremy.Held@web.de>
2025-06-18 19:47:22 +0200

    Deleted dynamo DB Workflow in TF

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,3 @@
-# DynamoDB Table for Terraform State Locking
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform State Lock Table"
-    Environment = var.environment
-    Project     = var.project_name
-  }
-}
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
